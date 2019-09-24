c2ff133feeebac6ecf7222ff3bbbd9b39487ddc6
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-24 01:06:14 -0300

    Adding endpoint in order to fetch a people list from the star wars API.

--- features/node/sample-app/src/app.js
+++ features/node/sample-app/src/app.js
@@ -2,14 +2,27 @@
  * Required modules.
  */
 const express = require('express');
+const axios = require('axios');
 
 
 // Create the express app.
 const app = express();
 const port = 3000;
 
+// Const variables.
+const SWAPI_URL = 'https://swapi.co/api';
+
 // Endpoints.
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/people', async (req, res, next) => {
+  try {
+    // SWAPI returns a json formatted content by default.
+    const people = await axios.get(`${SWAPI_URL}/people`);
+    res.send(people.data);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Start the server.
 app.listen(port, () => console.log(`Sample app listening on port ${port}!`));
