27ed2b63bb2c0cd8806fe8e567b9ab421121209f
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-24 02:02:47 -0300

    Adding endpoint in order to perform a POST and storage a person within a JSON file.

--- features/node/sample-app/src/app.js
+++ features/node/sample-app/src/app.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 
 // Create the express app.
@@ -34,6 +35,29 @@ app.get('/people/:id', async (req, res, next) => {
     // SWAPI returns a json formatted content by default.
     const people = await axios.get(`${SWAPI_URL}/people/${id}`);
     res.send(people.data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post('/people', async (req, res, next) => {
+  try {
+    // This opens up the writeable stream to people.js.
+    const writeStream = fs.createWriteStream('./src/people.json');
+
+    // Write the POST data to the file.
+    writeStream.write(JSON.stringify(req.body));
+
+    // In case of any errors occurs.
+    writeStream.on('error', (error) => next(error));
+
+    // Close the writable stream.
+    writeStream.end();
+
+    // After all the data is saved, respond with a simple message within an object.
+    writeStream.on('finish', () => res.send({
+      message: 'A new person has been successfully added to the star wars world.',
+    }));
   } catch (error) {
     next(error);
   }
