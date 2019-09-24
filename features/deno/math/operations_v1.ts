a48140e365e7735534e6b5580384c1914e647158
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-23 21:41:39 -0300

    Adding subtract operation for the math feature in its first version.

--- features/deno/math/operations_v1.ts
+++ features/deno/math/operations_v1.ts
@@ -7,10 +7,20 @@
  */
 const add = (num1: number, num2: number): number => num1 + num2;
 
+/**
+ * Subtract two numbers.
+ *
+ * @param {number} num1   The first number to perform the operation.
+ * @param {number} num2   The second number to perform the operation.
+ * @return {number}       The subtraction of the two assigned numbers.
+ */
+const subtract = (num1: number, num2: number): number => num1 - num2;
+
 
 /**
  * Export all the available methods.
  */
 export {
   add,
+  subtract,
 };
