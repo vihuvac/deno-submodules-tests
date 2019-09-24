1149c588aedda0c892539f344419ac7b9b5f231b
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-23 21:40:04 -0300

    Adding math feature with a single operation in its first version.

---
+++ features/deno/math/operations_v1.ts
@@ -0,0 +1,16 @@
+/**
+ * Add two numbers.
+ *
+ * @param {number} num1   The first number to perform the operation.
+ * @param {number} num2   The second number to perform the operation.
+ * @return {number}       The addition of the two assigned numbers.
+ */
+const add = (num1: number, num2: number): number => num1 + num2;
+
+
+/**
+ * Export all the available methods.
+ */
+export {
+  add,
+};
