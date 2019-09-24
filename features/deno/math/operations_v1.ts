b1e4da241ec5d8778f35f89a5265c2c26d9885bf
Víctor Hugo Valle Castillo <victouk@gmail.com>
2019-09-23 21:42:52 -0300

    Adding divide operation for the math feature in its first version.

--- features/deno/math/operations_v1.ts
+++ features/deno/math/operations_v1.ts
@@ -25,6 +25,15 @@ const subtract = (num1: number, num2: number): number => num1 - num2;
  */
 const multiply = (num1: number, num2: number): number => num1 * num2;
 
+/**
+ * Divide two numbers.
+ *
+ * @param {number} num1   The first number to perform the operation.
+ * @param {number} num2   The second number to perform the operation.
+ * @return {number}       The division of the two assigned numbers.
+ */
+const divide = (num1: number, num2: number): number => num1 / num2;
+
 
 /**
  * Export all the available methods.
@@ -33,4 +42,5 @@ export {
   add,
   subtract,
   multiply,
+  divide,
 };
